eddebf07fa862d9aa72c577b7be1a7135ed59590
Guilherme Thales <guilherme.simoes@alpes.one>
2018-10-01 21:31:54 -0300

    using props to share values to component

---
+++ public/ex.3/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './component';
+
+
+ReactDOM.render(<Component nome="Guilherme"/>, document.getElementById('app'));
