170ba5c0c7a6cfb3db8ac52d1912fe84bf59f1db
Guilherme Thales <guilherme.simoes@alpes.one>
2018-10-29 21:11:00 -0300

    backend completed

--- todo-app/backend/src/api/todo/todoService.js
+++ todo-app/backend/src/api/todo/todoService.js
@@ -3,4 +3,4 @@ const Todo = require('./todo');
 Todo.methods(['get', 'post', 'put', 'delete']);
 Todo.updateOptions({ new: true, runValidators: true });
 
-module.exports - Todo;
+module.exports = Todo;
--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -3,12 +3,15 @@ const config = {
   port: 3003
 };
 const bodyParser = require('body-parser');
-
 const server = express();
+const allowCors = require('./cors');
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(config.port, function() {
   console.log(`BACKEND is running on port ${config.port}`);
 });
+
+module.exports = server;
