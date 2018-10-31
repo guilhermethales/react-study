a8e29f53a114f1b642d16037d3062cea953a678b
Guilherme Thales <guilherme.simoes@alpes.one>
2018-10-30 21:57:09 -0300

    add Todo component

---
+++ todo-app/frontend/src/components/todo/Todo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Todo</h1>
+      </div>
+    )
+  }
+}
+
+export default Todo;
