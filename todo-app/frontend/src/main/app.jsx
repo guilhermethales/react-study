a3ba433d296f815b4078cafa036a1dbb8145807a
Guilherme Thales <guilherme.simoes@alpes.one>
2018-10-30 22:05:04 -0300

    add menu and about components

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -3,9 +3,13 @@ import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
 import Todo from '../components/todo/Todo';
+import About from '../components/about/About';
+import Menu from '../template/Menu';
 
 export default props => (
   <div className="container">
+    <Menu />
     <Todo />
+    <About />  
   </div>
 )
